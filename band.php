ffdba413e34bff6627c94afb9f70db1cca256ab7
Candace <2100978@talnet.nl>
2020-02-06 13:19:42 +0100

    Uitwerking van de exercise

---
+++ band.php
@@ -0,0 +1,18 @@
+<?php
+
+
+$Overzicht = [
+
+    "Citizen of Glass kost" => 4.5,
+    "Night kost " => 9,
+    "New Eyes kost " => 5,
+    "Strange Trails kost" =>  10,
+
+
+
+];
+
+
+
+echo "Het totaalbedrag an ale albums is " . array_sum($Overzicht) . "\n";
+echo "De Gemiddelde prijs van alle alle albums is " . array_sum($Overzicht) / count($Overzicht) . "\n";
